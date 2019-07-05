b162ad6cae42b96777c3aef519284a2c2fbbde4c
Peace-Song <driveshoot@snu.ac.kr>
2019-07-05 17:32:46 +0900

    changed Main.java to PN solver

--- Main.java
+++ Main.java
@@ -2,6 +2,24 @@ import java.util.*;
 
 class Main{
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.println("List of prime numbers:");
+        int i = 2;
+        int linePrintCount = 0;
+        while(true){
+            int count = 0;
+            
+            for(int j = 1; j <= i; j++)
+                if(i % j == 0) count++;
+
+            if(count == 2){
+                if(linePrintCount == 8){
+                    System.out.println("");
+                    linePrintCount = 0;
+                }
+                System.out.print(" | "+ i + "\t");
+                linePrintCount++;
+            }
+            i++;
+        }
     }
 }
