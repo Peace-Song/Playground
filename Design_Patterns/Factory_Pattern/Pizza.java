08588214020b1d700cc660ee8b6baab3d0ab43e3
peacesong <peacesong@miridih.com>
2020-07-25 00:33:52 +0900

    25JUL20

---
+++ Design_Patterns/Factory_Pattern/Pizza.java
@@ -0,0 +1,14 @@
+public class Pizza {
+    public void prepare() {
+
+    }
+    public void bake() {
+
+    }
+    public void cut() {
+
+    }
+    public void box() {
+
+    }
+}
---
+++ Design_Patterns/Factory_Pattern/PizzaStore.java
@@ -0,0 +1,48 @@
+public abstract class PizzaStore {
+    public final Pizza orderPizza(String type) {
+        Pizza pizza;
+
+        pizza = createPizza(type);
+        
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+
+    protected abstract Pizza createPizza(String type);
+}
+
+class NYPizzaStore extends PizzaStore {
+    protected Pizza createPizza(String item) {
+        if (item.equals("cheese")) {
+            return new NYStyleCheesePizza();
+        } else if (item.equals("veggie")) {
+            return new NYStyleVeggiePizza();
+        } else if (item.equals("clam")) {
+            return new NYStyleClamPizza();
+        } else if (item.equals("pepperoni")) {
+            return new NYStylePepperoniPizza();
+        } else {
+            return null;
+        }
+    }
+}
+
+class ChicagoPizzaStore extends PizzaStore {
+    protected Pizza createPizza(String item) {
+        if (item.equals("cheese")) {
+            return new ChicagoStyleCheesePizza();
+        } else if (item.equals("veggie")) {
+            return new ChicagoStyleVeggiePizza();
+        } else if (item.equals("clam")) {
+            return new ChicagoStyleClamPizza();
+        } else if (item.equals("pepperoni")) {
+            return new ChicagoStylePepperoniPizza();
+        } else {
+            return null;
+        }
+    }
+}
